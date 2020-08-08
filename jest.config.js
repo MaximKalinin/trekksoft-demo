8346a6b9bbeb894c3651c1b6721854919122c620
Maksim Kalinin <maksim@MacBook-Pro-Maksim-3.local>
2020-08-08 15:39:34 +0300

    fix: test pipeline

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
     "^@app/(.*)$": "<rootDir>/src/$1",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__test__/mocks__/fileMock.js",
+    "\\.(css|less)$": "identity-obj-proxy",
   },
 };
