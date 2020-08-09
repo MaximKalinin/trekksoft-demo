c0abb368490bb414026bf56db37d5c054908210a
Maksim Kalinin <maksim@MacBook-Pro-Maksim-3.local>
2020-08-09 18:44:27 +0300

    feat: added css-modules

--- typings.d.ts
+++ typings.d.ts
@@ -2,3 +2,8 @@ declare module "*.svg" {
   const resource: string;
   export default resource;
 }
+
+declare module "*.less" {
+  const resource: { [key: string]: string };
+  export default resource;
+}
