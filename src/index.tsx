3aaf9e951772377fa3c64f75308db2234049b64d
Maksim Kalinin <maksim@MacBook-Pro-Maksim-3.local>
2020-08-09 14:07:34 +0300

    refactor: removed server

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-(async () => {
-  const response = await fetch("/api/hello");
-  console.log(response);
-})();
+const github = axios.create({
+  baseURL: "https://api.github.com/",
+});
 
 const App: FC = () => {
-  return <div>hello, world!</div>;
+  const [query, setQuery] = useState("");
+  return (
+    <div>
+      hello, world!
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      <button
+        disabled={!query}
+        onClick={async () => {
+          const response = await github.get(
+            `/search/users?per_page=1000&page=1&q=${encodeURIComponent(query)}`
+          );
+          github.get("/rate_limit");
+          console.log(response);
+        }}
+      >
+        search
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
